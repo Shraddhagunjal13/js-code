a138eadb326a11127e37d2d3b04bd1945485b6c2
Shraddha Gunjal <85820035+Shraddhagunjal13@users.noreply.github.com>
2024-12-14 11:50:53 +0000

    datatype

---
+++ javascript/datatype.js
@@ -0,0 +1,25 @@
+"use strict"//treat all js code as newer version
+//alert(3+3)//we are using nodejs, not browser
+
+console.log(3 + 3)
+console.log("shraddha")
+ 
+let name="shraddha"
+let age = 25
+let isloggedIn = false
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//numm => standalone value
+//undefine =>
+//symbol => unique
+
+
+//object
+
+console.log(typeof "shraddha" );//string
+console.log(typeof age);//number
+console.log(typeof null);//object
+console.log(typeof undefined);//undfine
