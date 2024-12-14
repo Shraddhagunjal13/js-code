bc9ec500a189f00aaebac7f840311a498806c936
Shraddha Gunjal <85820035+Shraddhagunjal13@users.noreply.github.com>
2024-12-14 12:19:14 +0000

    Conversion of datatype

---
+++ javascript/ConversionOperation.js
@@ -0,0 +1,32 @@
+/* convrsion */
+let score= "null"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" =>NAN
+//"null" => 0
+//undefine => NAN
+//string "shraddha" => NAN
+//true => 1; false=> 0
+
+/* Boolean value conversion*/
+let isloggedIn = "shraddha"
+
+let booleanIsloggedIn = Boolean(isloggedIn)
+console.log(booleanIsloggedIn);
+
+// 1=> true; 0=> false
+//empty string ""=> true
+//"shraddha" => true
+
+ /* string conversion */
+let somenumber = 33
+let stringNumber = String(somenumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
+
