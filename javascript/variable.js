37ec4d3f3d6bbd10cbd41f68bc2a816f609f65fa
Shraddha Gunjal <85820035+Shraddhagunjal13@users.noreply.github.com>
2024-12-14 11:30:40 +0000

    js-code

---
+++ javascript/variable.js
@@ -0,0 +1,16 @@
+const accountId = 123443 /* use this */
+let accountEmail = "shraddha.gunjal13@gmail.com" /* use this */
+var accountPassword = "12345" /*Not use this */
+accountCity ="Sangamner" /*Not use this */
+let accountstate;
+
+//accountId = 2//not allowed
+accountEmail="sg@12.com"
+accountPassword="234455"
+accountCity="Nashik"
+console.log(accountId);
+/*
+prefer not use var
+Because of issue in block scope and functional scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstate]);
